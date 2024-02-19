27d259a069ae07bc2e01b9eddcb9ab6ca4bb68b4
Tommaso Visconti <tommaso.visconti@gmail.com>
2024-02-19 01:06:08 +0100

    charybdis: disable trackball if !pointer layer
    In bastardkb/charybdis/4x6 via keymap, disable the pointer (trackball)
    on all layers except in the pointer layer.
    
    As the auto sniping feature was automatically enabled on that layer,
    this commit also disables that feature. To use sniping now it's
    required to press the sniping key.

--- keyboards/bastardkb/charybdis/4x6/keymaps/via/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/via/keymap.c
@@ -28,7 +28,7 @@ enum charybdis_keymap_layers {
 };
 
 /** \brief Automatically enable sniping-mode on the pointer layer. */
-#define CHARYBDIS_AUTO_SNIPING_ON_LAYER LAYER_POINTER
+// #define CHARYBDIS_AUTO_SNIPING_ON_LAYER LAYER_POINTER
 
 #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 static uint16_t auto_pointer_layer_timer = 0;
@@ -119,31 +119,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 #ifdef POINTING_DEVICE_ENABLE
-#    ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+// #    ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+// report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+//     if (abs(mouse_report.x) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD || abs(mouse_report.y) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
+//         if (auto_pointer_layer_timer == 0) {
+//             layer_on(LAYER_POINTER);
+// #        ifdef RGB_MATRIX_ENABLE
+//             rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
+//             rgb_matrix_sethsv_noeeprom(HSV_GREEN);
+// #        endif // RGB_MATRIX_ENABLE
+//         }
+//         auto_pointer_layer_timer = timer_read();
+//     }
+
+//     return mouse_report;
+// }
+
+// void matrix_scan_user(void) {
+//     if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(), auto_pointer_layer_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
+//         auto_pointer_layer_timer = 0;
+//         layer_off(LAYER_POINTER);
+// #        ifdef RGB_MATRIX_ENABLE
+//         rgb_matrix_mode_noeeprom(RGB_MATRIX_DEFAULT_MODE);
+// #        endif // RGB_MATRIX_ENABLE
+//     }
+// }
+// #    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    if (abs(mouse_report.x) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD || abs(mouse_report.y) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
-        if (auto_pointer_layer_timer == 0) {
-            layer_on(LAYER_POINTER);
-#        ifdef RGB_MATRIX_ENABLE
-            rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
-            rgb_matrix_sethsv_noeeprom(HSV_GREEN);
-#        endif // RGB_MATRIX_ENABLE
-        }
-        auto_pointer_layer_timer = timer_read();
+    // Disable mouse movement when not in the pointer layer.
+    if (!layer_state_is(LAYER_POINTER)) {
+        mouse_report.x = 0;
+        mouse_report.y = 0;
+        mouse_report.v = 0;
+        mouse_report.h = 0;
     }
+
     return mouse_report;
 }
-
-void matrix_scan_user(void) {
-    if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(), auto_pointer_layer_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
-        auto_pointer_layer_timer = 0;
-        layer_off(LAYER_POINTER);
-#        ifdef RGB_MATRIX_ENABLE
-        rgb_matrix_mode_noeeprom(RGB_MATRIX_DEFAULT_MODE);
-#        endif // RGB_MATRIX_ENABLE
-    }
-}
-#    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
 #    ifdef CHARYBDIS_AUTO_SNIPING_ON_LAYER
 layer_state_t layer_state_set_user(layer_state_t state) {
