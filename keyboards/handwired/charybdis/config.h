3f64c946cb5fba1a4be9f000c7d64804953eb3e8
Tommaso Visconti <tommaso.visconti@gmail.com>
2024-04-04 23:30:23 +0200

    use new keyboard

---
+++ keyboards/handwired/charybdis/config.h
@@ -0,0 +1,29 @@
+// Copyright 2024 Tommaso Visconti (@tommyblue)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+#define MASTER_RIGHT
+
+/* Reset. */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+
+// #define SERIAL_PIO_USE_PIO0
+// #define SERIAL_USART_TX_PIN GP1
